3d144ffa602fd57da91d20e1a78e722091e7a82a
PierMario <piermario.salis@aol.com>
2021-02-15 23:06:50 +0100

    End

---
+++ resources/views/oop.blade.php
@@ -0,0 +1,52 @@
+<!-- Istruzioni:
+Create una classe per descrivere un entitá, assegnatele degli attributi, utilizzate il constructor. Create alcune istanze della classe. Stampate a schermo gli attributi di ogni istanza.
+Per chi non ha fantasia ecco qualche entitá che potete usare:
+Animale, Persona, Casa, Computer, Hotel, Film, Videogioco, Sport, Bicicletta, Veicolo, Nazione etc. -->
+@php
+
+class Superheroes {
+    public $nome;
+    public $universo;
+    
+    function __construct($nome, $universo) {
+        $this->nome = $nome;
+        $this->universo = $universo;
+    }
+
+
+
+
+
+}
+
+echo '<h1>Super Heroes</h1>';
+
+echo ' <strong>Name:</strong> '.$nome = 'Batman <br>' ;
+echo ' <strong>Universe:</strong> '.$universo = 'DC COMICS <br> <br> ' ;
+
+echo ' <strong>Name:</strong> '.$nome = 'SuperMan <br>';
+echo ' <strong>Universe:</strong> '.$universo = 'DC COMICS <br> <br> ' ;
+
+echo ' <strong>Name:</strong> '.$nome = 'Robin <br>';
+echo ' <strong>Universe:</strong> '.$universo = 'DC COMICS <br> <br> ' ;
+
+echo ' <strong>Name:</strong> '.$nome = 'Wolverine <br>';
+echo ' <strong>Universe:</strong> '.$universo = 'MARVEL <br> <br> ' ;
+
+echo ' <strong>Name:</strong> '.$nome = 'Thor <br>';
+echo ' <strong>Universe:</strong> '.$universo = 'MARVEL <br> <br> ' ;
+
+echo ' <strong>Name:</strong> '.$nome = 'Captain America <br>';
+echo ' <strong>Universe:</strong> '.$universo = 'MARVEL <br> <br> ' ;
+
+echo ' <strong>Name:</strong> '.$nome = 'Iron Man <br>';
+echo ' <strong>Universe:</strong> '.$universo = 'MARVEL <br> <br> ' ;
+
+echo ' <strong>Name:</strong> '.$nome = 'Hulk <br>';
+echo ' <strong>Universe:</strong> '.$universo = 'MARVEL <br> <br> ' ;
+
+
+
+
+
+@endphp
